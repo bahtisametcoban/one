c04e3b73ee6720587ce52fcffa9ee9c80793b6b5
Bahtiyar Samet Coban <bahtiyarsametcoban@Bahtiyars-MacBook-Air.local>
2017-10-17 13:39:35 +0300

    added constraint of  input number should be distinct numbers

--- src/main/java/com/one/dto/request/NumberPredictRequestDto.java
+++ src/main/java/com/one/dto/request/NumberPredictRequestDto.java
@@ -8,12 +8,31 @@ import lombok.Data;
 @Builder
 public class NumberPredictRequestDto {
     private Long number;
+    private char[] numberArray;
+
 
     public boolean isNumberValid() {
-        return number >= NumberEnum.MIN_NUMBER.getNumber() && number <= NumberEnum.MAX_NUMBER.getNumber();
+        numberArray= numberToCharArray(number);
+        boolean isValid=true;
+        if (number >= NumberEnum.MIN_NUMBER.getNumber() && number <= NumberEnum.MAX_NUMBER.getNumber()) {
+            for (int i = 0; i < numberArray.length; i++) {
+                for (int j = i + 1; j < numberArray.length; j++) {
+                    if (numberArray[i] == numberArray[j]) {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+        }
+        return isValid;
     }
 
     public boolean isNumberNotValid() {
         return !isNumberValid();
+    }
+
+    private char[] numberToCharArray(Long number) {
+        return number.toString().toCharArray();
     }
 }
